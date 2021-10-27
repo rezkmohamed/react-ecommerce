5afa659a209434ee512cf8aa0bd224f49f846b9e
rezkmohamed <momito310399@gmail.com>
2021-10-27 15:32:06 +0200

    refacto Login and Register styles

--- src/components/pages/auth/Login.js
+++ src/components/pages/auth/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "./Login.module.css";
 
 const Login = () => {
@@ -17,29 +18,29 @@ const Login = () => {
                         <form>
                         <div className={`${classes['inpyut-group']} ${classes['mb-3']}`}>
                 <div className={classes['input-group-prepend']}>
-                <span className={classes['input-group-text']}><i class="fa fa-user"></i></span>
+                <span className={classes['input-group-text']}><i className="fa fa-user"></i></span>
             </div>
             <input type="text" className={classes['form-control']} placeholder="Email" />
             </div>
             <div className={`${classes['input-group']} ${classes['mb-3']}`}>
                 <div className={classes['input-group-prepend']}>
-                <span className={classes['input-group-text']}><i class="fa fa-lock"></i></span>
+                <span className={classes['input-group-text']}><i className="fa fa-lock"></i></span>
             </div>
             <input type="text" className={classes['form-control']} placeholder="Password" />
             </div>
-            <button type="button" className={`${classes.btn} ${classes['btn-secondary']} ${classes['btn-block']}`}>LOGIN</button>
+            <button type="button" className={`${classes.button} ${classes['btn-secondary']} ${classes['btn-block']}`}>LOGIN</button>
             <div className={classes.message}>
             {/* <div><input type="checkbox" /> Remember ME</div> */}
-            <div><a href="#">Reset password</a></div>
-            <div><a href="#">Registrati!!!!</a></div>
+            <div><Link to="/">Reset password</Link></div>
+            <div><Link to="/register">Registrati!!!!</Link></div>
 
             </div>
                 </form>
-                        <div className={classes.social}>
+                        {/* <div className={classes.social}>
                         <a href="#"><i className="fab fa-facebook"></i></a>
                         <a href="#"><i className="fab fa-twitter-square"></i></a>
                         <a href="#"><i className="fab fa-google"></i></a>
-                        </div>
+                        </div> */}
                     </div>
                     </div>
         </div>)
--- src/components/pages/auth/Register.js
+++ src/components/pages/auth/Register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "./Register.module.css";
 
 const Register = () => {
@@ -17,29 +18,39 @@ const Register = () => {
                         <form>
                         <div className={`${classes['inpyut-group']} ${classes['mb-3']}`}>
                 <div className={classes['input-group-prepend']}>
-                <span className={classes['input-group-text']}><i class="fa fa-user"></i></span>
+                <span className={classes['input-group-text']}><i className="fa fa-user"></i></span>
             </div>
             <input type="text" className={classes['form-control']} placeholder="Email" />
             </div>
             <div className={`${classes['input-group']} ${classes['mb-3']}`}>
                 <div className={classes['input-group-prepend']}>
-                <span className={classes['input-group-text']}><i class="fa fa-lock"></i></span>
+                <span className={classes['input-group-text']}><i className="fa fa-lock"></i></span>
             </div>
             <input type="text" className={classes['form-control']} placeholder="Password" />
             <input type="text" className={classes['form-control']} placeholder="Conferma Password" />
             </div>
-            <button type="button" className={`${classes.btn} ${classes['btn-secondary']} ${classes['btn-block']}`}>REGISTRA</button>
+            <div>
+                <input type="radio" id="huey" name="drone" value="huey" defaultChecked />
+                <label htmlFor="huey">Acquirente</label>
+            </div>
+
+            <div>
+                <input type="radio" id="dewey" name="drone" value="dewey" />
+                <label html="dewey">Venditore</label>
+            </div>
+
+            <button type="button" className={`${classes.button} ${classes['btn-secondary']} ${classes['btn-block']}`}>REGISTRA</button>
             <div className={classes.message}>
             {/* <div><input type="checkbox" /> Remember ME</div> */}
-            <div><a href="#">Vai a Login</a></div>
+            <div><Link to="/login">Vai a Login</Link></div>
 
             </div>
                 </form>
-                        <div className={classes.social}>
-                        <a href="#"><i className="fab fa-facebook"></i></a>
+                        {/* <div className={classes.social}>
+                        <Link href="#"><i className="fab fa-facebook"></i></a>
                         <a href="#"><i className="fab fa-twitter-square"></i></a>
                         <a href="#"><i className="fab fa-google"></i></a>
-                        </div>
+                        </div> */}
                     </div>
                     </div>
         </div>)
