89bb1f98766187102f7c7a5d1bb88b89a43a71b0
rezkmohamed <momito310399@gmail.com>
2021-10-27 11:26:48 +0200

    added Routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Footer from './components/UI/Footer';
 import Header from './components/UI/Header';
+import React from 'react';
+import { Route } from 'react-router';
+import LandingPage from './components/pages/LandingPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Header />
-      sasiasdiasdjioasjioasji
-      <br />
-      <Footer />
-    </div>
+    <React.Fragment>
+      <Route path="/" exact component={LandingPage} />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/pages/LandingPage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Footer from '../UI/Footer';
+import Header from '../UI/Header';
+
+const LandingPage = (props) => {
+    return (
+        <div>
+            <Header />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <Footer />
+        </div>
+    );
+}
+
+export default LandingPage;
