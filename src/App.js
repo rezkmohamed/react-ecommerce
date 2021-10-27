9a9cedeca11a3d0f100686da195b83b21b27f776
rezkmohamed <momito310399@gmail.com>
2021-10-27 11:47:31 +0200

    added Gallery UI component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import Footer from './components/UI/Footer';
-import Header from './components/UI/Header';
 import React from 'react';
 import { Route } from 'react-router';
 import LandingPage from './components/pages/LandingPage';
--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,40 +1,13 @@
 import React from 'react';
 import Footer from '../UI/Footer';
+import Gallery from '../UI/Gallery';
 import Header from '../UI/Header';
 
 const LandingPage = (props) => {
     return (
         <div>
             <Header />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            <Gallery />
 
             <Footer />
         </div>
