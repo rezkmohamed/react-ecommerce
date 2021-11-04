f977cc3c5a805a21331b36671e1cc97fd41d6b2a
rezkmohamed <momito310399@gmail.com>
2021-11-04 14:26:50 +0100

    refactor Gallery's style

--- src/components/UI/Gallery.js
+++ src/components/UI/Gallery.js
@@ -17,92 +17,27 @@ const Gallery = ({products, isLoading}) => {
                     !isLoading && 
                     products.map(prod => {
                         return (
-                        <Link
-                        to={`/detail/?idprod=${prod.idProduct}`}
-                        key={prod.idProduct}
-                        data-lightbox="models"
-                        data-title="Caption1"
-                        >
-                        <img
-                            src={prod.img}
-                            alt="foto rappresentativa del prodotto"
-                        />
-                        </Link>)
+                            <div>
+                                <Link
+                                to={`/detail/?idprod=${prod.idProduct}`}
+                                key={prod.idProduct}
+                                data-lightbox="models"
+                                data-title="Caption1"
+                                >
+                                <img
+                                    src={prod.img}
+                                    alt="foto rappresentativa del prodotto"
+                                />
+                                </Link>
+                                <div className={classes['product-info']}>
+                                    <p>product: {prod.name}</p>
+                                    <p>
+                                        price: $ {prod.price}
+                                    </p>
+                                </div>
+                            </div>)
                         })
                 }
-
-                {/* <a
-                href="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                data-lightbox="models"
-                data-title="Caption1"
-                >
-                <img
-                    src="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                />
-                </a>
-                <a
-                href="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                data-lightbox="models"
-                data-title="Caption2"
-                >
-                <img
-                    src="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                />
-                </a>
-                <a
-                href="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                data-lightbox="models"
-                data-title="Caption3"
-                >
-                <img
-                    src="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                />
-                </a>
-                <a
-                href="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                data-lightbox="models"
-                data-title="Caption4"
-                >
-                <img
-                    src="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                />
-                </a>
-                <a
-                href="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                data-lightbox="models"
-                data-title="Caption5"
-                >
-                <img
-                    src="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                />
-                </a>
-                <a
-                href="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                data-lightbox="models"
-                data-title="Caption6"
-                >
-                <img
-                    src="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                />
-                </a>
-                <a
-                href="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                data-lightbox="models"
-                data-title="Caption7"
-                >
-                <img
-                    src="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                />
-                </a>
-                <a
-                href="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                data-lightbox="models"
-                data-title="Caption8"
-                >
-                <img
-                    src="https://m.media-amazon.com/images/I/718s2f1qRWL._AC_UX679_.jpg"
-                />
-                </a> */}
             </div>
             </div>
         </body>
